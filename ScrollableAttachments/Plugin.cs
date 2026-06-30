18b469af8d467e85c40857769b582486763e15cc
peinwastaken <legokid173@gmail.com>
2026-06-30 16:46:56 +0300

    increment version

--- ScrollableAttachments/Plugin.cs
+++ ScrollableAttachments/Plugin.cs
@@ -5,7 +5,7 @@ using SPT.Reflection.Patching;
 
 namespace AttachmentScrolling;
 
-[BepInPlugin("com.pein.attachmentscrolling", "ScrollableAttachments", "1.0.0")]
+[BepInPlugin("com.pein.attachmentscrolling", "ScrollableAttachments", "1.1.0")]
 public class Plugin : BaseUnityPlugin
 {
     internal static new ManualLogSource Logger;
